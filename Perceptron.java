007e6fc96797a69b0993447399c7c37497da9dde
eliasubz <eliasls2002@yahoo.de>
2022-12-13 00:22:35 +0100

    Finished

--- Perceptron.java
+++ Perceptron.java
@@ -1,11 +1,10 @@
 import java.util.Arrays;
 
-import javax.sound.sampled.SourceDataLine;
-
 public class Perceptron {
     private static double error = 0;
-    private static double m = 0.1; // Learning Constant
+    private static double learningConstant = 0.1; // Learning Constant
     private static double range = 1; // Range
+    private static double[] weights;
     private static double d = 0; // deltaWeight
     private static double sumError = 1; // sum_error
     private static double someValue = 0.05; // some_value
@@ -14,51 +13,54 @@ public class Perceptron {
     private static int[] targetOutputsAND = { 0, 0, 0, 1 };
     private static double[][] trainDataOR = { { 0, 0, 1 }, { 1, 0, 1 }, { 0, 1, 1 }, { 1, 1, 1 } };
     private static int[] targetOutputsOR = { 0, 1, 1, 1 };
-    private static double[][] trainDataLinear = { { 0, 0, 1 }, { 1, 1, 1 }, { 2, 2, 1 }, { 3, 3, 1 }, { 0, 1.1, 1 },
+    private static double[][] trainDataLinear = { { 0, 0.9, 1 }, { 3, 7.1, 1 }, { 5, 10.9, 1 },{ 0, 0, 1 }, { 1, 1, 1 }, { 2, 2, 1 }, { 3, 3, 1 }, { 0, 1.1, 1 },
             { 1, 2, 1 }, { 2, 3, 1 }, { 3, 4, 1 }, { 0, -1, 1 }, { 1, 0, 1 }, { 2, 1, 1 }, { 3, 2, 1 },
-            { 0, -2, 1 }, { 1, -1, 1 }, { 2, 0, 1 }, { 3, 1, 1 } , { 3, 122, 1 } , { 3, 6.5, 1 } , { -12, -46.5, 1 },{-50,-101,1},{100,202,1}};
-    private static int[] targetOutputsLinear = { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,1,0,0,0,1};
+            { 0, -2, 1 }, { 1, -1, 1 }, { 2, 0, 1 }, { 3, 1, 1 }, { 3, 122, 1 }, { 3, 6.5, 1 }, { -12, -18, 1 },
+            { -50, -101, 1 }, { 100, 202, 1 } };
+    private static int[] targetOutputsLinear = { 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1 };
 
     private static double[][] testDataLinear = { { 0, 0, 1 }, { 1, 1, 1 }, { 2, 2, 1 }, { 3, 3, 1 },
-            { 0, 1, 1 }, { 1, 2, 1 }, { 2, 4.8, 1 }, { 3, 6.8, 1 },
+            { 0, 0.5, 1 }, { 1, 2, 1 }, { 2, 4.8, 1 }, { 3, 6.8, 1 },
             { 0, -1, 1 }, { 1, 0, 1 }, { 2, 1, 1 }, { 3, 2, 1 },
             { 0, -2, 1 }, { 1, -1, 1 }, { 2, 0, 1 }, { 3, 1, 1 },
             { 0, 2, 1 }, { 1, 4, 1 }, { 2, 6, 1 }, { 3, 8, 1 },
             { 0, 7.2, 1 }, { 1, 3.1, 1 }, { 2, 5.5, 1 }, { 3, 12, 1 } };
-    private static int[] testOutputsLinear = { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
+    private static int[] testOutputsLinear = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
 
-    private static double[][] trainDataLinearMin = { { 0, 0.9, 1 }, { 5, 10.9, 1 }, { 0, 1.1, 1 }, { 5, 11.1, 1 } };
-    private static int[] targetOutputsLinearMin = { 0, 0, 1, 1 };
-    
+
+
+    private static double[][] trainDataLinearMin = { { 0, 0.9, 1 }, { 3, 7.1, 1 }, { 5, 10.9, 1 } };
+    private static int[] targetOutputsLinearMin = { 0, 1, 0 };
 
     public static void main(String[] args) {
-        System.out.println("this is the average: " + avarage(20));
+        System.out.println("this is the average: " + avarage(20,trainDataLinearMin,targetOutputsLinearMin));
+        // System.out.println("this is the average: " + avarage(1,trainDataLinear,targetOutputsLinear));
 
+        System.out.print("this is the totalerror with the testData: " + totalError(testDataLinear, weights, testOutputsLinear));
     }
-    public static double avarage(int k){
+
+    public static double avarage(int k, double[][] trainData, int[] targetOutputs) {
         String str = "";
         double result = 0;
-        for (int i = 0;i < k; i++){
-            int b =  NN();
+        for (int i = 0; i < k; i++) {
+            int b = NN(trainData, targetOutputs);
             str += b + ", ";
             result += b;
         }
         System.out.println(str);
-        return result/k;
+        return result / k;
     }
-    public static int NN (){
-        double[][] trainData = trainDataAND;
-        int[] targetOutputs = targetOutputsAND;
 
+    public static int NN(double[][] trainData, int[] targetOutputs) {
 
         int activation = 0; // activation
 
-        double[] weights = initializeWeights(3, range); // Initialize weights at random
+        weights = initializeWeights(3, range); // Initialize weights at random
         boolean optimumFound = false;
 
         int counter = 0;
 
-        for (int iterator = 0;!optimumFound; iterator++) {
+        for (int iterator = 0; !optimumFound; iterator++) {
 
             while (sumError > someValue && sumError <= previousValue) { // while sum_error is still getting much smaller
 
@@ -67,15 +69,14 @@ public class Perceptron {
                 activation = calculateActivation(trainData, weights, iterator);
                 error = targetOutputs[iterator] - activation;
                 // System.out.println("this will indicate the direction "+error);
-//                System.out.println("iteration: " + iteration);
+                // System.out.println("iteration: " + iteration);
                 sumError += Math.abs(error);
-//                System.out.println("this is sumerror: " + sumError);
+                // System.out.println("this is sumerror: " + sumError);
 
                 if (sumError != 0) {
                     for (int i = 0; i < weights.length; i++) {
-                        d = m * error * trainData[iterator][i];
+                        d = learningConstant * error * trainData[iterator][i];
                         weights[i] += d;
-                        // System.out.println("d: " + d);
                     }
                     System.out.println("Adjusted weights " + Arrays.toString(weights));
 
@@ -92,7 +93,8 @@ public class Perceptron {
                     System.out.println("Total Optimum was found!!!");
                     System.out.println("It took " + counter + " adjustments");
                     System.out.println(
-                            "The weights are " + weights[0] + " and " + weights[1] + " and the weight of the bias unit is " + weights[2]);
+                            "The weights are " + weights[0] + " and " + weights[1]
+                                    + " and the weight of the bias unit is " + weights[2]);
 
                 } else {
                     System.out.println("Optimum wasn't found yet and iterator was reset");
@@ -108,7 +110,7 @@ public class Perceptron {
         // System.out.print("this is the totalerror with the testData: ");
         // System.out.println(totalError(trainDataLinear, w, targetOutputsLinear));
 
-        //System.out.println("Counter: " + counter);
+        // System.out.println("Counter: " + counter);
         return counter;
     }
 
@@ -119,7 +121,7 @@ public class Perceptron {
         for (int i = 0; i < inputs[iteration].length; i++) {
             netInput += inputs[iteration][i] * weights[i];
         }
-//        System.out.println("this is netInput: " + netInput);
+        // System.out.println("this is netInput: " + netInput);
         if (netInput <= 0) { // Activation stepfunction at t = 0;
             a = 0;
         } else {
@@ -144,7 +146,13 @@ public class Perceptron {
         int activation = 0;
         for (int i = 0; i < inputs.length; i++) {
             activation = calculateActivation(inputs, weights, i);
-            totalSumError += Math.abs(targetOutputs[i] - activation);
+            int er = Math.abs(targetOutputs[i] - activation);
+            if (er > 0){
+                System.out.println("position: " + i);
+                System.out.println("activation: " + activation);
+                System.out.println("inputs: " + Arrays.toString(inputs[i]));
+            }
+            totalSumError += er;
         }
 
         return totalSumError;
